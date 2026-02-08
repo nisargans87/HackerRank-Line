7ad163f32e2924073a867bff32eface5005ecc12
NISARGA NS <nisargans011@gmail.com>
2026-02-08 22:16:45 +0530

    Create Problem_1.py

---
+++ Python/Problem_1.py
@@ -0,0 +1,11 @@
+# Task - Print "Hello, World!" 
+
+# A sample line of code that can be executed in Python:
+print("Hello, World!")
+
+# You can just as easily store a string as a variable and then print it to stdout:
+my_string = "Hello, World!"
+print(my_string)
+# The above code will print Hello, World! on your screen (Hello, World!)
+
+# Pypy 3 or python 3 available in Hackerrank 
