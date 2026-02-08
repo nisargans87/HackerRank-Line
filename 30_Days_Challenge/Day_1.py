b77008dc35bd21fcefbec502d9485e00b5335984
NISARGA NS <nisargans011@gmail.com>
2026-02-08 22:33:46 +0530

    Create Day_1.py

---
+++ 30_Days_Challenge/Day_1.py
@@ -0,0 +1,25 @@
+'''Task - 
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.'''
+''' Input Format -
+A single line of text denoting  (the variable whose contents must be printed).
+Output Format -
+Print Hello, World. on the first line, and the contents of  on the second line.
+Sample Input -
+Welcome to 30 Days of Code!
+Sample Output -
+Hello, World. 
+Welcome to 30 Days of Code!
+Explanation - 
+On the first line, we print the string literal Hello, World.. On the second line, we print the contents of the input_string variable which, for this sample case, happens to be Welcome to 30 Days of Code!. If you do not print the variable's contents to stdout, you will not pass the hidden test case.'''
+
+# code starts here 
+input_string = input()
+print("Hello ! , World. ")
+print(input_string)
+# code ends here 
+ 
+''' Input (stdin)
+Welcome to 30 Days of Code!'''
+''' Output 
+Hello, World.
+Welcome to 30 Days of Code!'''
