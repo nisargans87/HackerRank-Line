ccc3362362e170217acbe69f054b9c8afd3190b7
NISARGA NS <nisargans011@gmail.com>
2026-02-09 22:13:03 +0530

    Implement performOperation function for task
    Fix parsefloat typo and improve output formatting.

---
+++ 10_Days_Challenge/Day_2.js
@@ -0,0 +1,26 @@
+/** Task
+Variables named firstInteger, firstDecimal and firstString are declared for you in the editor below. You must use the '+' operator to perform the following sequence of operations:
+Convert secondInteger  to an integer (Number type), then sum it with firstInteger  and print the result on a new line using console.log.
+Convert secondDecimal to a floating-point number (Number type), then sum it with firstDecimal and print the result on a new line using console.log.
+Print the concatenation of firstString  and secondString  on a new line using console.log. Note that firstString  must be printed first. **/
+
+function performOperation(secondInteger, secondDecimal, secondString){
+  const firstInteger = 4;
+  const firstDecimal = 4.0 ; 
+  const firstString = 'HackerRank' ; 
+  console.log(firstInteger + parseInt(secondInteger));
+  console.log(firstDecimal + parsefloat(secondDecimal));
+  console.log(firstString + (secondString ));
+
+  /** Output 
+Input (stdin) -
+12
+4.32
+is the best place to learn and practice coding!
+Expected Output -
+16
+8.32
+HackerRank is the best place to learn and practice coding! **/
+  
+  
+  
