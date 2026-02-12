7aa50481f70ae1269759ae753087f23e1a740997
NISARGA NS <nisargans011@gmail.com>
2026-02-12 22:26:41 +0530

    Update and rename Problem_1.py to Task-1.py

---
+++ Python/Task-1.py
@@ -0,0 +1,8 @@
+# Task - To Print "Hello, World!" 
+
+# method -1 
+print("Hello, World!")
+
+# method-2 , You can just  easily store a value  in the my_string variable and then print it to stdout:
+my_string = "Hello, World!"
+print(my_string)
